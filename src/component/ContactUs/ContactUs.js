c5c956ad89ce1f4c0ca4ae420c4c2757c24dbb46
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-05-17 16:55:16 +0600

    setup

---
+++ src/component/ContactUs/ContactUs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ContactUs.css";
+
+const ContactUs = () => {
+  return <section>contact us</section>;
+};
+
+export default ContactUs;
---
+++ src/component/mailChimp/MainChimp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainChimp = () => {
+  return <div>MainChimp</div>;
+};
+
+export default MainChimp;
