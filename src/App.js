5a8e13200d54c6d4b279c90bc3c7d36d04a0a69b
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-10-02 16:32:24 +0600

    messenger package added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import Subscribe from "./component/Subscribe";
 import CatDetails from "./component/Fake_data/CatDetails";
 import ContactUs from "./component/ContactUs/ContactUs";
 import MultyDropdown from "./component/MultyDropdown/Dropdown";
+// import MessengerCustomerChat from "react-messenger-customer-chat/lib/MessengerCustomerChat";
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/magnify" element={<Magnify />} />
-      <Route path="/subscribe" element={<Subscribe />} />
-      <Route path="/cats" element={<CatDetails />} />
-      <Route path="/contact" element={<ContactUs />} />
-      <Route path="/menus" element={<MultyDropdown />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/magnify" element={<Magnify />} />
+        <Route path="/subscribe" element={<Subscribe />} />
+        <Route path="/cats" element={<CatDetails />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/menus" element={<MultyDropdown />} />
+      </Routes>
+      {/* <div>
+        <MessengerCustomerChat
+          pageId="248491315296285"
+          appId="1083660138985344"
+          htmlRef="<html>HI</html>"
+        />
+      </div> */}
+    </>
   );
 }
 
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Popup from "./Popup";
+import "./Home.css";
 
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import Header from "./Header";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [buttonPopup, setButtonPopup] = useState(false);
@@ -39,6 +41,16 @@ function Home() {
         <h1>Popup After Page Load</h1>
         <p>Hello world</p>
       </Popup>
+      <div>
+        FB
+        <a href="m.me/106332790962587" target="_blank">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Facebook_Messenger_logo_2020.svg/1200px-Facebook_Messenger_logo_2020.svg.png"
+            alt=""
+            className="messenger"
+          />
+        </a>
+      </div>
     </div>
   );
 }
